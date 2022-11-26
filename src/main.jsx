1ed7ae38048633ea3dafbf8f35259c43bc05ea91
PaisMariano <mariano_a_p@hotmail.com>
2022-11-26 18:55:14 -0300

    added font menti to proyect

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./utils/theme"; 
+import Fonts from "./utils/fonts";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App/>
+      <Fonts />
+        <App/>
     </ChakraProvider>
   </React.StrictMode>
 )
